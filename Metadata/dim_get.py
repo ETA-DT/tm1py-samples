390be38e8cbcf33b72804750b2874cc55ce2fff2
ETA-DT <165398598+ETA-DT@users.noreply.github.com>
2024-04-08 18:18:40 +0200

    modifies sync_dimension

--- Metadata/dim_get.py
+++ Metadata/dim_get.py
@@ -28,18 +28,70 @@ with TM1Service(**config['tm1srv01']) as tm1:
         raise SystemExit(f'No dimension named {dimension_name}')
     dimension = tm1.dimensions.get(dimension_name=dimension_name)
 
-    # iterate through hierarchies
-    for hierarchy in dimension:
-        print('Hierarchy Name: {}'.format(hierarchy.name))
-        # iterate through Elements in hierarchy
-        for element in hierarchy:
-            print('Element Name: {} Index: {} Type: {}'.format(element.name, str(element.index), element.element_type))
-        # iterate through Subsets
-        for subset in hierarchy.subsets:
-            print('Subset Name: {}'.format(subset))
-        # iterate through Edges
-        for parent, child in hierarchy.edges:
-            print("Parent Name: {}, Component Name: {}".format(parent, child))
+    with open(f'../Outputs/{dimension_name}_info.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
 
-        # print the default member
-        print('Default Member: {}'.format(hierarchy.default_member))
+        # iterate through hierarchies
+        for hierarchy in dimension:
+            writer.writerow(['Hierarchy Name: {}'.format(hierarchy.name)])
+            print('Hierarchy Name: {}'.format(hierarchy.name))
+            for parent, child in hierarchy.edges:
+                writer.writerow([child, parent])
+            print(hierarchy.edges)
+            list_edges = [(val2,val1) for (val1,val2) in hierarchy.edges]
+            par = [val[0] for val in hierarchy.edges]
+            chi = [val[1] for val in hierarchy.edges]
+            writer.writerow(list_edges)
+            for couple in list_edges:
+                if couple[0] in par:
+                    print(couple[1])
+            break
+            
+
+            # # iterate through Elements in hierarchy
+            # for element in hierarchy:
+            #     writer.writerow(['Element Name: {} Index: {} Type: {}'.format(element.name, str(element.index), element.element_type)])
+            #     print(['Element Name: {} Index: {} Type: {}'.format(element.name, str(element.index), element.element_type)])
+
+            # # iterate through Subsets
+            # for subset in hierarchy.subsets:
+            #     writer.writerow(['Subset Name: {}'.format(subset)])
+            #     print('Subset Name: {}'.format(subset))
+
+            # # iterate through Edges
+            # for parent, child in hierarchy.edges:
+            #     writer.writerow(["Parent Name: {}, Component Name: {}".format(parent, child)])
+            #     parent_child = {}
+            #     print("Parent Name: {}, Component Name: {}".format(parent, child))
+            
+            # # print the default member
+            # writer.writerow(['Default Member: {}'.format(hierarchy.default_member)])
+            # print('Default Member: {}'.format(hierarchy.default_member))
+
+        #     start = 0
+        #     current_level = start
+        #     current_type = 'Consolidated'
+        #     previous_type = current_type[:]
+        #     # iterate through Elements in hierarchy
+        #     for element in hierarchy:
+        #         current_type = element.element_type
+        #         if str(previous_type) == 'Consolidated':
+        #             current_level += 1
+        #         else:
+        #             if str(current_type) == 'Consolidated':
+        #                 current_level -= 1
+        #         level_string = '--'*current_level
+        #         previous_type = current_type
+        #         writer.writerow([level_string + element.name])
+
+        # all_cubes = tm1.cubes.get_all()
+        # cube_with_dimension = []
+        # for cube in all_cubes:
+        #     if dimension_name in cube.dimensions:
+        #         print(cube.name)
+        #         cube_with_dimension.append(cube.name)
+        
+        # writer.writerow([])
+        # writer.writerow([f'Cubes containing {dimension_name} dimension'])
+        # for cube_name in cube_with_dimension:
+        #     writer.writerow([cube_name])
--- Metadata/sync_dimension_and_subsets.py
+++ Metadata/sync_dimension_and_subsets.py
@@ -1,30 +1,47 @@
+import configparser
 import time
+import os
 
 from TM1py import TM1Service
 
-ADDRESS = "localhost"
-SSL = True
-USER = "admin"
-PASSWORD = "apple"
+def set_current_directory():
+    abspath = os.path.abspath(__file__)         # file absolute path
+    directory = os.path.dirname(abspath)        # current file parent directory
+    os.chdir(directory)
+    return directory
 
-tm1_master = TM1Service(address=ADDRESS, port=12354, ssl=SSL, user=USER, password=PASSWORD)
-tm1_other = TM1Service(address=ADDRESS, port=12297, ssl=SSL, user=USER, password=PASSWORD)
+CURRENT_DIRECTORY = set_current_directory()
 
-while True:
-    dimension_master = tm1_master.dimensions.get(dimension_name="Region")
-    dimension_other = tm1_other.dimensions.get(dimension_name="Region")
+config = configparser.ConfigParser()
+config.read(r'..\config.ini')
 
-    if dimension_master != dimension_other:
-        print(f"Recognized changes. Updating dimension: '{dimension_master.name}'")
-        tm1_other.dimensions.update(dimension_master)
+tm1_master = TM1Service(**config['tm1srv01'])
+tm1_other = TM1Service(**config['tm1srv02'])
 
-    subsets_names = tm1_master.subsets.get_all_names("Region")
-    for subsets_name in subsets_names:
-        subset_master = tm1_master.subsets.get(subsets_name, dimension_name="Region")
+# while True:
+dimension_master = tm1_master.dimensions.get(dimension_name="dimension_test")
+dimension_other = tm1_other.dimensions.get(dimension_name="dimension_test")
 
-        subset_other = tm1_other.subsets.get(subsets_name, dimension_name="Region")
-        if subset_master != subset_other:
-            print(f"Recognized changes. Updating Subset: '{subsets_name}'")
-            tm1_other.subsets.update(subset_master)
+if dimension_master != dimension_other:
+    print(f"Recognized changes. Updating dimension: '{dimension_master.name}'")
+    tm1_other.dimensions.update(dimension_master)
 
-    time.sleep(2)
+subsets_names = tm1_master.subsets.get_all_names("dimension_test")
+for subsets_name in subsets_names:
+    subset_master = tm1_master.subsets.get(subsets_name, dimension_name="dimension_test")
+
+    subset_other = tm1_other.subsets.get(subsets_name, dimension_name="dimension_test")
+    if subset_master != subset_other:
+        print(f"Recognized changes. Updating Subset: '{subsets_name}'")
+        tm1_other.subsets.update(subset_master)
+
+hierarchy_names = tm1_master.hierarchies.get_all_names(dimension_name="dimension_test")
+for hierarchy_name in hierarchy_names:
+    hierarchy_master = tm1_master.hierarchies.get(dimension_name="dimension_test",hierarchy_name=hierarchy_name)
+
+    hierarchy_other = tm1_other.hierarchies.get(dimension_name="dimension_test",hierarchy_name=hierarchy_name)
+    if hierarchy_master.element_attributes != hierarchy_other.element_attributes:
+        print(f"Recognized changes. Updating Hierarchy: '{hierarchy_name}'")
+        tm1_other.hierarchies.update(hierarchy_master)
+    tm1_other.hierarchies.update_element_attributes(hierarchy_master,keep_existing_attributes=True)
+    # time.sleep(2)
